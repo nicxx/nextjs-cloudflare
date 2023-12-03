967bffb761586ced38baf59fad31069acd962fc9
Nick <vnick89@gmail.com>
2023-12-03 07:48:03 +0000

    test api

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -6,5 +6,15 @@ import type { NextRequest } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
+  return new Response(
+    JSON.stringify({
+      name: process.env.NEXT_RUNTIME,
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    },
+  );
 }
